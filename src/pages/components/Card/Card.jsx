181ff31c85b0935771d4ddff6f42d953856c8f0d
Unexperiance1student <75188379+Unexperiance1student@users.noreply.github.com>
2022-11-28 22:51:26 +0300

    a

--- src/pages/components/Card/Card.jsx
+++ src/pages/components/Card/Card.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { likePost } from '../../../store/slice/postsSlice';
 import './style.scss';
 
-export default function Card({ imgUrl }) {
+export default function Card({ imgUrl, likes, comments, isLikedByYou }) {
+  const dispatch = useDispatch();
+  const onClickLike = (authorizedUser, id) => {
+    dispatch(likePost({ userId: authorizedUser, postId: id }));
+  };
+
   return (
     <div className='cnCardRoot'>
       <img
@@ -9,6 +16,17 @@ export default function Card({ imgUrl }) {
         src={imgUrl}
         alt='aoaoa'
       />
+      <div className='cnCardHover' />
+      <div className='cnCardIcons'>
+        <i
+          className={`${
+            isLikedByYou ? 'fa-solid' : 'fa-regular'
+          } fa-heart cnCardIcon`}
+        />
+        <span className='cnCardNumber cnCardLikes'>{likes}</span>
+        <i className='fas fa-comment cnCardIcon' />
+        <span className='cnCardNumber'>{comments}</span>
+      </div>
     </div>
   );
 }
